e0081c52d7526260fc813ce4746ef06856551bc6
Kenneth Tomagan <kenneth.tomagan123@gmail.com>
2024-08-11 08:49:27 +0800

    API route refactor

--- routes/api.php
+++ routes/api.php
@@ -8,43 +8,42 @@ use App\Http\Controllers\Tickets\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::get("/me", fn(Request $request) => $request->user());
 
+    // Email Verification Routes
     Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['throttle:6,1'])
-    ->name('verification.send');
+        ->middleware(['throttle:6,1'])
+        ->name('verification.send');
 
+    // Auth Routes
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 
-    Route::get("/projects", [ProjectController::class, 'index']);
-    Route::post("/project", [ProjectController::class, 'store']);
-    Route::get("/project/{project}", [ProjectController::class, 'show']);
-    Route::put("/project/{project}", [ProjectController::class, 'update']);
-    Route::delete("/project/{project}", [ProjectController::class, 'destroy']);
+    // Project Routes
+    Route::prefix('project')->group(function () {
+        Route::get('/', [ProjectController::class, 'index']);
+        Route::post('/', [ProjectController::class, 'store']);
+        Route::get('/{project}', [ProjectController::class, 'show']);
+        Route::put('/{project}', [ProjectController::class, 'update']);
+        Route::delete('/{project}', [ProjectController::class, 'destroy']);
+    });
 
+    // Board Routes
+    Route::prefix('board')->group(function () {
+        Route::post('/', [BoardController::class, 'store']);
+        Route::put('/{board}', [BoardController::class, 'update']);
+        Route::delete('/{board}', [BoardController::class, 'destroy']);
+    });
 
-    // Route::get("project/{project}/boards", [BoardController::class, 'index']);
-
-    Route::post("/board", [BoardController::class, 'store']);
-    // Route::get("/board/{board}", [BoardController::class, 'show']);
-    Route::put("/board/{board}", [BoardController::class, 'update']);
-    Route::delete("/board/{board}", [BoardController::class, 'destroy']);
-
-
-    Route::post("/ticket", [TicketController::class, 'store']);
-    Route::get("/ticket/{ticket}", [TicketController::class, 'show']);
-    Route::put("/ticket/{ticket}", [TicketController::class, 'update']);
-    Route::delete("/ticket/{ticket}", [TicketController::class, 'destroy']);
-
-
-    Route::post("/ticket/{ticket}/move", [TicketController::class, 'move']);
-    Route::post("/ticket/{ticket}/assign", [TicketController::class, 'assign']);
+    // Ticket Routes
+    Route::prefix('ticket')->group(function () {
+        Route::post('/', [TicketController::class, 'store']);
+        Route::get('/{ticket}', [TicketController::class, 'show']);
+        Route::put('/{ticket}', [TicketController::class, 'update']);
+        Route::delete('/{ticket}', [TicketController::class, 'destroy']);
+        Route::post('/{ticket}/move', [TicketController::class, 'move']);
+        Route::post('/{ticket}/assign', [TicketController::class, 'assign']);
+    });
 });
